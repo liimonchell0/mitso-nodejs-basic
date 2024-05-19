ab75bcd6aa9ced2e16ec31bbe8dd5ede963443f3
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2024-05-19 17:00:01 +0300

    read text

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { existsSync, readFileSync } from 'fs';
+import { join } from 'path';
 const read = async () => {
-    // Write your code here 
+    const filePath = join('C:/RIS/mitso-nodejs-basic/src/fs/files', 'fileToRead.txt');
+
+  try {
+    // Проверяем, существует ли файл
+    if (existsSync(filePath)) {
+      const fileContent = readFileSync(filePath, 'utf8');
+      console.log("Содержание файла:");
+      console.log(fileContent);
+    } else {
+      throw new Error("FS operation failed");
+    }
+  } catch (err) {
+    console.error("Error:", err.message);
+  }
 };
 
 await read();
