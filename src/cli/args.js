68d7c3eb31d57b71411b6703d17270a8b43edd65
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2024-05-19 17:11:36 +0300

    cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,23 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2); // Получаем аргументы, начиная со 2-го
+    const options = {};
+  
+    // Проходим по аргументам и заполняем объект options
+    for (let i = 0; i < args.length; i += 2) {
+      const propName = args[i].slice(2); // Вырезаем "--" из начала ключа
+      const propValue = args[i + 1];
+      options[propName] = propValue;
+    }
+  
+    // Формируем строку для вывода
+    let output = "";
+    for (const propName in options) {
+      const propValue = options[propName];
+      output += `${propName} is ${propValue}\n`;
+    }
+  
+    // Выводим результат в консоль
+    console.log(output.trim());
 };
 
 parseArgs();
