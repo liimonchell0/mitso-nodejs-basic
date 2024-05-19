7d573b21e2da5fbfd897a0d023e396fe31a57169
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2024-05-19 16:53:01 +0300

    delete txt file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { existsSync, unlinkSync } from 'fs';
+import { join } from 'path';
 const remove = async () => {
-    // Write your code here 
+    const filePath = join('C:/RIS/mitso-nodejs-basic/src/fs/files', 'fileToRemove.txt');
+
+    try {
+      // Проверяем, существует ли файл
+      if (existsSync(filePath)) {
+        unlinkSync(filePath);
+        console.log("File deleted successfully!");
+      } else {
+        throw new Error("FS operation failed");
+      }
+    } catch (err) {
+      console.error("Error:", err.message);
+    }
 };
 
 await remove();
