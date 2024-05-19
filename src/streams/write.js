96f26f10393f77f39e40b0f2787830e2664749a3
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2024-05-19 18:17:23 +0300

    write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+import path from 'path';
 const write = async () => {
-    // Write your code here 
+    const filePath = path.join('C:/RIS/mitso-nodejs-basic/src/streams/files', 'fileToWrite.txt');
+
+    const writeStream = fs.createWriteStream(filePath);
+
+    // Обработка потока данных из stdin
+    process.stdin.pipe(writeStream);
+
 };
 
 await write();
