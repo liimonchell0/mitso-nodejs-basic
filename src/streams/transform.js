c816a5efcae88e827a67334bc325b045be1b9a49
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2024-05-19 18:23:40 +0300

    srtreams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { Transform } from 'stream';
+
+class ReverseTransform extends Transform {
+    constructor(options) {
+        super(options);
+    }
+
+    _transform(chunk, encoding, callback) {
+        // Переворачиваем строку
+        const reversedChunk = chunk.toString().split('').reverse().join('');
+        this.push(reversedChunk);
+        callback();
+    }
+}
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new ReverseTransform();
+
+    // Подключаем потоки: stdin -> transform -> stdout
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
