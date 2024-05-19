1d8a02f9c76de169e5cd4a17ee676008fe3852c4
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2024-05-19 17:56:58 +0300

    calc

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import path from 'path';
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join('C:/RIS/mitso-nodejs-basic/src/hash/files', 'fileToCalculateHashFor.txt');
+    
+    try {
+        const fileBuffer = fs.readFileSync(filePath);
+        const hashSum = crypto.createHash('sha256');
+        hashSum.update(fileBuffer);
+        const hex = hashSum.digest('hex');
+        console.log(`SHA256 hash of the file is: ${hex}`);
+    } catch (error) {
+        console.error('Error calculating hash:', error);
+    }
 };
 
 await calculateHash();
