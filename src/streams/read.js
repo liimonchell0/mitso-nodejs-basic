68a096b4401665968f6feaf373e66312ead32c56
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2024-05-19 18:00:03 +0300

    read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join('C:/RIS/mitso-nodejs-basic/src/streams/files', 'fileToRead.txt');
+
+    const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('error', (err) => {
+        console.error('Error reading the file:', err);
+    });
+
+    readStream.on('end', () => {
+        console.log('\nFile reading completed.');
+    });
 };
 
 await read();
