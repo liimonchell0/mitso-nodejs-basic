d3d62f605ed7d617b4984fdc9b176ee2a886222a
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2024-05-19 18:47:28 +0300

    decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
 const decompress = async () => {
-    // Write your code here 
+    const archivePath = path.join('C:/RIS/mitso-nodejs-basic/src/zip/files', 'archive.gz');
+    const filePath = path.join('C:/RIS/mitso-nodejs-basic/src/zip/files', 'fileToCompress.txt');
+
+    const readStream = fs.createReadStream(archivePath);
+    const writeStream = fs.createWriteStream(filePath);
+    const gunzip = zlib.createGunzip();
+
+    readStream.pipe(gunzip).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('File has been decompressed successfully.');
+    });
+
+    writeStream.on('error', (err) => {
+        console.error('Error decompressing file:', err);
+    });
 };
 
 await decompress();
