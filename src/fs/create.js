c2cab6ee096085571a5948ba0cb9714923fdce48
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2024-05-19 15:47:03 +0300

    create.js add

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+
 const create = async () => {
-    // Write your code here 
+  try {
+     const filePath = 'C:/RIS/mitso-nodejs-basic/src/fs/files/fresh.txt'
+       const fileContent = 'I am fresh and young!';
+       await fs.promises.writeFile(filePath, fileContent);
+
+    console.log('Новый файл успешно создан.');
+  } catch (error) {
+    console.error('Ошибка при создании файла:', error);
+  }
 };
 
 await create();
+//создает новый файл
