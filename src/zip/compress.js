b104dc7af4275f83e1a6d65701ad141d2e18b91c
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2024-05-19 18:44:37 +0300

    archive

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+
 const compress = async () => {
-    // Write your code here 
+    
+    const filePath = path.join('C:/RIS/mitso-nodejs-basic/src/zip/files', 'fileToCompress.txt');
+    const archivePath = path.join('C:/RIS/mitso-nodejs-basic/src/zip/files', 'archive.gz');
+
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream(archivePath);
+    const gzip = zlib.createGzip();
+
+    readStream.pipe(gzip).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('File has been compressed successfully.');
+    });
+
+    writeStream.on('error', (err) => {
+        console.error('Error compressing file:', err);
+    });
 };
 
-await compress();
+await compress('fileToCompress.txt', 'archive.gz');
