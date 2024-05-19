919fd3db49c4b7298070bd5ef49999cb95c313df
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2024-05-19 19:03:20 +0300

    cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    // Создаем дочерний процесс из файла script.js с переданными аргументами
+    const childProcess = spawn('node', ['C:/RIS/mitso-nodejs-basic/src/cp/files/script.js', ...args], {
+        stdio: ['pipe', 'pipe', 'pipe']
+    });
+
+    // Перенаправляем потоки между главным и дочерним процессами
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
+    childProcess.stderr.pipe(process.stderr);
+
+    // Ждем завершения дочернего процесса
+    childProcess.on('close', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+// Пример вызова функции с аргументами
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,43 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import path from 'path';
+
 const performCalculations = async () => {
-    // Write your code here
+    const numCores = os.cpus().length;
+    const workers = [];
+    const results = [];
+
+    for (let i = 0; i < numCores; i++) {
+        workers.push(
+            new Promise((resolve) => {
+                const worker = new Worker(path.resolve('C:/RIS/mitso-nodejs-basic/src/wt/', 'worker.js'));
+
+                worker.on('message', (data) => {
+                    resolve({ status: 'resolved', data });
+                });
+
+                worker.on('error', () => {
+                    resolve({ status: 'error', data: null });
+                });
+
+                worker.on('exit', (code) => {
+                    if (code !== 0) {
+                        resolve({ status: 'error', data: null });
+                    }
+                });
+
+                worker.postMessage(10 + i); // Отправляем возрастающий номер, начиная с 10
+            })
+        );
+    }
+
+    // Ожидаем выполнения всех worker и собираем результаты
+    const workerResults = await Promise.all(workers);
+
+    // Записываем результаты в массив
+    workerResults.forEach(result => results.push(result));
+
+    console.log(results);
 };
 
 await performCalculations();
